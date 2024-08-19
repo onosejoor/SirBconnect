e5a09df02c6eb9331b939d6536739eccc3f20ea1
Onos <onosejoor14@gmail.com>
2024-08-19 11:00:52 +0100

    Update Skills.jsx

--- src/components/sections/Skills.jsx
+++ src/components/sections/Skills.jsx
@@ -1,13 +1,18 @@
 import SkillDiv from "./SkillDiv";
 
 function Skills() {
-
   return (
     <section id="services">
       <h1 className="about">What We Offer</h1>
       <div className="arrow"></div>
       <div id="skill">
         <SkillDiv
+          src="/images/skills/scholarships.jpg"
+          header="Scholarships"
+          url="schorlarships"
+          text="At SirBconnect, we understand that finding the right school and scholarship can be challenging. Our scholarship consultation services are designed to help you navigate this process with ease. We provide expert advice and resources to help you identify schools that fit your budget and academic goals. Let us guide you in finding affordable education options and securing the best possible opportunities for your future."
+        />
+                <SkillDiv
           src="/images/skills/graphicDesign.jpg"
           header="Graphic designs and Trainings"
           url="graphic-design"
@@ -19,17 +24,12 @@ function Skills() {
           url="internet-services"
           text=" SirBconnect offers reliable internet services and comprehensive research solutions. Whether you need high-speed internet for your home or business, or in-depth research for academic or professional projects, we’ve got you covered. Our team ensures you stay connected and have access to the information you need."
         />
+
         <SkillDiv
           url="web-dev"
           src="/images/skills/research3.jpg"
           header=" Full-stack Web Development"
           text="Our web development team creates responsive, user-friendly websites tailored to your needs. Enhance your online presence with a professional website from SirBconnect. We specialize in creating websites that are not only visually appealing but also optimized for performance and user experience."
-        />
-        <SkillDiv
-          src="/images/skills/scholarships.jpg"
-          header="Scholarships"
-          url="schorlarships"
-          text="At SirBconnect, we understand that finding the right school and scholarship can be challenging. Our scholarship consultation services are designed to help you navigate this process with ease. We provide expert advice and resources to help you identify schools that fit your budget and academic goals. Let us guide you in finding affordable education options and securing the best possible opportunities for your future."
         />
 
         <SkillDiv
